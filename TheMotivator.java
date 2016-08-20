17216c14fb623db2876a11b51f75449cf49c0da3
monikakhanna <khn_monika@yahoo.com>
2016-08-19 17:06:21 -0700

    print score and change println

--- TheMotivator.java
+++ TheMotivator.java
@@ -1,5 +1,6 @@
 public class TheMotivator {
     public void scoreFeedback(int score) {
+        printScore(score)
         if (score == 100)
             System.out.println("You're awesome");
         else if (score > 90)
@@ -8,8 +9,9 @@ public class TheMotivator {
             System.out.println("That's better");
         else if (score > 60)
             System.out.println("That's good ");
-        else
-            System.out.println("Well, what can I say?");
+        else {
+            System.out.println("Needs Improvement");
+        }
     }
     public static void main(String[] args) {
         TheMotivator tm = new TheMotivator();
